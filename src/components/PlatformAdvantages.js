3c3ca902dc1e7776e295e13f5db3324799d4580f
shalinigandhi <shalinigandhi2@gmail.com>
2025-04-05 23:11:22 -0500

    Update image

--- src/components/PlatformAdvantages.js
+++ src/components/PlatformAdvantages.js
@@ -11,7 +11,7 @@ export default function PlatformAdvantages() {
       <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start lg:mt-[150px]">
         <img
           src="/images/business-meeting.svg"
-          className="w-full h-[219px] md:w-[520px] md:h-[340px] lg:w-[614px] lg:h-[397px] rounded-[25px] border-[6px] border-white mb-[40px] lg:mb-[0]"
+          className="w-full h-[219px] md:w-[520px] md:h-[340px] lg:w-[614px] lg:h-[397px] rounded-[25px] mb-[40px] lg:mb-[0]"
         />
         <div className="flex-grow lg:ml-[80px] w-full">
           <Tabs />
