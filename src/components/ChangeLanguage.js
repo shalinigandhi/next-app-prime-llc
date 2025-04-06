61aeaec0a274bec7fd102a3ff5a61280077cd242
shalinigandhi <shalinigandhi2@gmail.com>
2025-04-06 11:51:33 -0500

    Code cleanup

--- src/components/ChangeLanguage.js
+++ src/components/ChangeLanguage.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 const LanguageOption = ({ language, onClick, logo, altText }) => (
   <button
     onClick={() => onClick(language)}
-    className={`w-full text-left px-4 py-2 flex items-center cursor-pointer first:border-b first:border-[#4A4A4A]`}
+    className="w-full text-left px-4 py-2 flex items-center cursor-pointer first:border-b first:border-[#4A4A4A]"
   >
     <img src={logo} alt={altText} className="w-[24px] h-[24px] mr-[10px]" />
     {language === "EN" ? "English" : "Greek"}
@@ -20,22 +20,28 @@ export default function ChangeLanguage() {
     setDropdownOpen(false);
   };
 
+  const buttonClasses =
+    "px-4 py-2 text-white flex items-center border border-solid rounded-[39px] border-[#94949480] focus:outline-none cursor-pointer text-[12px] lg:text-[14px]";
+  const imgClasses = "w-[24px] h-[24px] mr-[10px]";
+  const dropdownClasses =
+    "absolute mt-2 right-0 w-32 bg-black text-white font-medium text-[14px] rounded-[14px] border border-solid border-[#94949480] z-[1] cursor-pointer";
+
   return (
     <div className="relative mr-[8px] hidden sm:block">
       <button
-        className="px-4 py-2 text-white flex items-center border border-solid rounded-[39px] border-[#94949480] focus:outline-none cursor-pointer text-[12px] lg:text-[14px]"
+        className={buttonClasses}
         onClick={() => setDropdownOpen(!dropdownOpen)}
       >
         <img
           src={language === "EN" ? "/images/english.svg" : "/images/greek.svg"}
           alt={language === "EN" ? "English Logo" : "Greek Logo"}
-          className="w-[24px] h-[24px] mr-[10px]"
+          className={imgClasses}
         />
         {language}
         <img src="/images/arrow-down.svg" alt="Dropdown Arrow" />
       </button>
       {dropdownOpen && (
-        <div className="absolute mt-2 right-0 w-32 bg-black text-white font-medium text-[14px] rounded-[14px] border border-solid border-[#94949480] z-[1] cursor-pointer">
+        <div className={dropdownClasses}>
           <LanguageOption
             language="EN"
             onClick={handleLanguageChange}
