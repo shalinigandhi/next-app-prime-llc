50d94866b13de539921074e58aac7bc035e5ab16
shalinigandhi <shalinigandhi2@gmail.com>
2025-04-05 23:05:33 -0500

    Minor fix

--- src/components/PlatformAdvantages.js
+++ src/components/PlatformAdvantages.js
@@ -8,7 +8,7 @@ export default function PlatformAdvantages() {
         highlightWidth="md:w-[162px] w-[108px]"
         right="right-[108px] md:right-0"
       />
-      <div className="flex flex-col lg:flex-row justify-between items-center lg:mt-[150px]">
+      <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start lg:mt-[150px]">
         <img
           src="/images/business-meeting.svg"
           className="w-full h-[219px] md:w-[520px] md:h-[340px] lg:w-[614px] lg:h-[397px] rounded-[25px] border-[6px] border-white mb-[40px] lg:mb-[0]"
